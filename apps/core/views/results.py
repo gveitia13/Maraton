4ce91751d7678e00c5db8c57b90c17eba6a9da01
Gilbert Veitia Urbay <61420914+gveitia13@users.noreply.github.com>
2023-10-12 15:27:07 -0400

    filters ready

--- apps/core/views/results.py
+++ apps/core/views/results.py
@@ -35,11 +35,16 @@ class ResultUpdate(LoginRequiredMixin, generic.UpdateView):
 class ResultList(LoginRequiredMixin, generic.ListView):
     model = Result
     template_name = 'result/list.html'
+    queryset = Result.objects.all().order_by('-date')
 
     def get_queryset(self):
         qs = super().get_queryset()
         if self.request.GET.get('search'):
-            return qs.filter(title__icontains=self.request.GET.get('search'))
+            qs = qs.filter(name__icontains=self.request.GET.get('search'))
+        if self.request.GET.get('year'):
+            qs = qs.filter(date__year=self.request.GET.get('year'))
+        if self.request.GET.get('category'):
+            qs = qs.filter(category=self.request.GET.get('category'))
         return qs
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -48,6 +53,15 @@ class ResultList(LoginRequiredMixin, generic.ListView):
         context['add_url'] = reverse_lazy('result-create')
         context['update_url'] = '/result-update/'
         context['delete_url'] = '/result-delete/'
+        context['years'] = [i.date.year for i in self.queryset]
+        context['categories'] = (
+            ('m', 'Maratón'),
+            ('mm', 'Media Maratón'),
+            ('10', '10 km'),
+            ('msr', 'Maratón silla de ruedas'),
+            ('mmsr', 'Medio Maratón silla de ruedas'),
+            ('10sr', '10 km silla de ruedas'),
+        )
         return context
 
 
